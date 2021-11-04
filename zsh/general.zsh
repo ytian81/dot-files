c4c14d1dd0d802fd3731c89b0dd743387724463f
Yang Tian <ytian.gt@gmail.com>
2021-11-04 00:45:47 -0700

    [vim] consistent fzf layout between zsh and vim

--- zsh/general.zsh
+++ zsh/general.zsh
@@ -13,9 +13,10 @@ export FZF_DEFAULT_OPTS='
   --color fg:#ebdbb2,bg:#282828,hl:#fabd2f,fg+:#ebdbb2,bg+:#3c3836,hl+:#fabd2f
   --color info:#83a598,prompt:#bdae93,spinner:#fabd2f,pointer:#83a598,marker:#fe8019,header:#665c54
   --layout=reverse
-  --height 40%
+  --height 80%
   --border
   --bind ctrl-b:preview-half-page-up,ctrl-f:preview-half-page-down,ctrl-/:toggle-preview
+  --preview-window=right,60%
 '
 export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --ignore-vcs'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
