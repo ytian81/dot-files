0a08eaa0395d7500a48d391f72edd896be3d09f2
Yang Tian <ytian.gt@gmail.com>
2022-05-13 00:01:26 -0700

    [zshrc] always show commit

--- zsh/p10k.zsh
+++ zsh/p10k.zsh
@@ -384,7 +384,7 @@
 
     # Display the current Git commit if there is no branch or tag.
     # Tip: To always display the current Git commit, remove `[[ -z $where ]] &&` from the next line.
-    [[ -z $where ]] && res+="${meta}@${clean}${VCS_STATUS_COMMIT[1,8]}"
+    res+="${meta}@${clean}${VCS_STATUS_COMMIT[1,8]}"
 
     # Show tracking branch name if it differs from local branch.
     if [[ -n ${VCS_STATUS_REMOTE_BRANCH:#$VCS_STATUS_LOCAL_BRANCH} ]]; then
