f6cd53920ea46eddfe533d675116776530ef291d
Yang Tian <ytian.gt@gmail.com>
2022-10-04 23:02:52 -0700

    [zsh] use ohmyzsh for fzf-extras

--- zsh/general.zsh
+++ zsh/general.zsh
@@ -7,8 +7,6 @@
 # fi
 
 # fzf
-[ -f ~/.fzf-extras/fzf-extras.sh ] && source ~/.fzf-extras/fzf-extras.sh
-[ -f ~/.fzf-extras/fzf-extras.zsh ] && source ~/.fzf-extras/fzf-extras.zsh
 export FZF_DEFAULT_OPTS='
   --color fg:#ebdbb2,bg:#282828,hl:#fabd2f,fg+:#ebdbb2,bg+:#3c3836,hl+:#fabd2f
   --color info:#83a598,prompt:#bdae93,spinner:#fabd2f,pointer:#83a598,marker:#fe8019,header:#665c54
