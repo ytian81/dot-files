6fa61117e920e3d10f7c97d1089bce459870c2d2
Yang Tian <ytian.gt@gmail.com>
2023-06-18 23:37:43 -0700

    [tmux] udpate notify-iterm2.sh

--- tmux/notify-iterm2.sh
+++ tmux/notify-iterm2.sh
@@ -17,8 +17,17 @@ esc="\e]9;$buf\a"
 # printf "$esc"
 
 # otherwise, we need to wire the content to the right tty ourselves
-esc="\ePtmux;\e$esc\e\\"
-pane_active_tty=$(tmux list-panes -F "#{pane_active} #{pane_tty}" | awk '$1=="1" { print $2 }')
-target_tty="${SSH_TTY:-$pane_active_tty}"
+if [ -n "${TMUX-}" ]; then
+  # wrap tmux pass through escape sequence
+  esc="\ePtmux;\e$esc\e\\"
+  # set to tmux pane tty
+  target_tty=$(tmux list-panes -F "#{pane_active} #{pane_tty}" | awk '$1=="1" { print $2 }')
+else
+  # set to normal tty
+  target_tty=$(tty)
+fi
+
+# set to SSH_TTY if in a SSH session
+target_tty="${SSH_TTY:-$target_tty}"
 
 printf "$esc" > "$target_tty"
