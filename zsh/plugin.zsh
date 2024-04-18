4ccccfa3c5789ddb199b52f78330029556e2b77f
Yang Tian <ytian.gt@gmail.com>
2024-04-17 23:26:05 -0700

    [zsh] use `VI_MODE_SET_CURSOR` and `VI_MODE_CURSOR_INSERT` to change cursor in zsh vi-mode

--- zsh/plugin.zsh
+++ zsh/plugin.zsh
@@ -11,26 +11,8 @@ bindkey -M menuselect 'l' vi-forward-char
 bindkey -M menuselect 'j' vi-down-line-or-history
 bindkey -v '^?' backward-delete-char
 
-# Change cursor shape for different vi modes.
-function zle-keymap-select {
-  if [[ ${KEYMAP} == vicmd ]] ||
-     [[ $1 = 'block' ]]; then
-    echo -ne '\e[2 q'
-  elif [[ ${KEYMAP} == main ]] ||
-       [[ ${KEYMAP} == viins ]] ||
-       [[ ${KEYMAP} = '' ]] ||
-       [[ $1 = 'beam' ]]; then
-    echo -ne '\e[5 q'
-  fi
-}
-zle -N zle-keymap-select
-zle-line-init() {
-    zle -K viins # initiate `vi insert` as keymap (can be removed if `bindkey -V` has been set elsewhere)
-    echo -ne "\e[5 q"
-}
-zle -N zle-line-init
-echo -ne '\e[5 q' # Use beam shape cursor on startup.
-preexec() { echo -ne '\e[5 q' ;} # Use beam shape cursor for each new prompt.
+VI_MODE_SET_CURSOR=true
+VI_MODE_CURSOR_INSERT=5
 
 # Edit line in vim with ctrl-o:
 autoload edit-command-line; zle -N edit-command-line
