41a500dccfcc60d218ff3b95f0cb0b01f805dca6
Yang Tian <ytian.gt@gmail.com>
2024-04-17 23:10:04 -0700

    [zsh] cleanup

--- zsh/plugin.zsh
+++ zsh/plugin.zsh
@@ -1,6 +1,7 @@
 # vi-mode
-# 10ms for key sequences
-KEYTIMEOUT=1
+# 10ms for key sequences for faster mode transition in zsh vi-mode
+KEYTIMEOUT=1 # in hundredth of a second
+
 # Use vim keys in tab complete menu:
 zstyle ':completion:*' menu select
 zmodload zsh/complist
@@ -9,6 +10,7 @@ bindkey -M menuselect 'k' vi-up-line-or-history
 bindkey -M menuselect 'l' vi-forward-char
 bindkey -M menuselect 'j' vi-down-line-or-history
 bindkey -v '^?' backward-delete-char
+
 # Change cursor shape for different vi modes.
 function zle-keymap-select {
   if [[ ${KEYMAP} == vicmd ]] ||
@@ -30,7 +32,7 @@ zle -N zle-line-init
 echo -ne '\e[5 q' # Use beam shape cursor on startup.
 preexec() { echo -ne '\e[5 q' ;} # Use beam shape cursor for each new prompt.
 
-# Edit line in vim with ctrl-e:
+# Edit line in vim with ctrl-o:
 autoload edit-command-line; zle -N edit-command-line
 bindkey '^o' edit-command-line
 
