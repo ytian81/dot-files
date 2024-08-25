7b8687d00380260579dc2c3bbd46a267d7819135
Yang Tian <ytian.gt@gmail.com>
2024-08-24 22:49:17 -0700

    [install] update font install scripts

--- install/macos/install_fonts.sh
+++ install/macos/install_fonts.sh
@@ -5,20 +5,21 @@ IFS=$'\n\t'
 
 # if homebrew not detected, install homebrew first
 
-# install fonts via homebrew
-brew tap homebrew/cask-fonts
-brew install --cask font-source-code-pro
+# install source code pro
+brew install font-source-code-pro
 
-# install fonts via directly downloading
-# https://github.com/romkatv/powerlevel10k#how-was-the-recommended-font-created
-function install_fonts_via_directly_downloading() {
-  mkdir -p -- ~/Library/Fonts
-  local -r font_base_url='https://github.com/romkatv/powerlevel10k-media/raw/master'
-  local style
-  for style in Regular Bold Italic 'Bold Italic'; do
-    local file="MesloLGS NF ${style}.ttf"
-    curl -fsSL -o ~/Library/Fonts/$file.tmp "$font_base_url/${file// /%20}"
-    mv -f -- ~/Library/Fonts/$file{.tmp,} || quit -c
-  done
-}
-install_fonts_via_directly_downloading
+# install nerdfonts
+# # install fonts via directly downloading
+# # https://github.com/romkatv/powerlevel10k#how-was-the-recommended-font-created
+# function install_fonts_via_directly_downloading() {
+#   mkdir -p -- ~/Library/Fonts
+#   local -r font_base_url='https://github.com/romkatv/powerlevel10k-media/raw/master'
+#   local style
+#   for style in Regular Bold Italic 'Bold Italic'; do
+#     local file="MesloLGS NF ${style}.ttf"
+#     curl -fsSL -o ~/Library/Fonts/$file.tmp "$font_base_url/${file// /%20}"
+#     mv -f -- ~/Library/Fonts/$file{.tmp,} || quit -c
+#   done
+# }
+# install_fonts_via_directly_downloading
+brew install font-meslo-lg-nerd-font
