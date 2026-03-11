4a3cb7b3f1cada495038ad1d47e41eddcb657b23
Yang Tian <ytian.gt@gmail.com>
2026-03-10 23:29:21 -0700

    [zshrc] use .local/bin for zsh

--- zsh/general.zsh
+++ zsh/general.zsh
@@ -2,9 +2,9 @@
 # echo "Loading general setting"
 
 # # set PATH so it includes user's private bin if it exists
-# if [ -d "$HOME/.local/bin" ] ; then
-#     PATH="$HOME/.local/bin:$PATH"
-# fi
+if [ -d "$HOME/.local/bin" ] ; then
+    PATH="$HOME/.local/bin:$PATH"
+fi
 
 # source fzf if not already
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
