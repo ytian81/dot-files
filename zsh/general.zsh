502e2e4e2d1ea51c783f0184bac41099050dde50
Yang Tian <ytian.gt@gmail.com>
2026-05-08 15:17:36 -0700

    [zsh] update fzf ui

--- zsh/general.zsh
+++ zsh/general.zsh
@@ -27,6 +27,7 @@ export FZF_DISPLAY_OPTS="--height 80% --tmux center,80%,80%"
 export FZF_DEFAULT_OPTS="
   --color fg:#ebdbb2,bg:#282828,hl:#fabd2f,fg+:#ebdbb2,bg+:#3c3836,hl+:#fabd2f
   --color info:#83a598,prompt:#bdae93,spinner:#fabd2f,pointer:#83a598,marker:#fe8019,header:#665c54,border:#928374
+  --style=full
   --layout=reverse
   --border
   --no-separator
@@ -36,6 +37,7 @@ export FZF_DEFAULT_OPTS="
   --bind='ctrl-/:change-preview-window(right,60%|hidden|down,60%)'
   --bind='ctrl-y:execute-silent(print {} | $YANK_SCRIPT)'
   --header=\"$(printf ':: Press \033[38;5;208mCTRL-Y\033[0m to yank, \033[38;5;208mCTRL-/\033[0m to toggle preview')\"
+  --header-border=inline
   --preview-window=down,60%
   $FZF_DISPLAY_OPTS
 "
